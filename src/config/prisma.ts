633e34f04d3a5840f3259a4ea311ec8c1ca19b89
dhz idl <40068123+7memyselfandi6@users.noreply.github.com>
2026-03-15 14:55:36 +0300

    Update prisma.ts

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,12 +1,12 @@
 import { PrismaClient } from '@prisma/client';
-// @ts-ignore
 import { PrismaPg } from '@prisma/adapter-pg';
 import pg from 'pg';
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
 const pool = new pg.Pool({ connectionString });
-const adapter = new PrismaPg(pool);
+// Use 'as any' to bypass the Pool type version mismatch
+const adapter = new PrismaPg(pool as any);
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
